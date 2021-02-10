0b9c17ae9c075d0e48bb7aea804e0fa7fd0c73d6
Jo <joannadawnmiller@gmail.com>
2021-02-10 10:13:55 -0500

    refactor API request to use fetch()

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
 
 function clearFields() {
   $('#location').val("");
@@ -10,17 +11,22 @@ function clearFields() {
   $('.showTemp').text("");
 }
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+  }
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
-  });
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
+  }); 
 });
