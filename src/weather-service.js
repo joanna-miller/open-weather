a45591d03dfc62b3d98701921b9d0770c7234aba
Jo <joannadawnmiller@gmail.com>
2021-02-10 10:36:28 -0500

    refactor to utilize async functions

--- src/weather-service.js
+++ src/weather-service.js
@@ -1,14 +1,14 @@
 export default class WeatherService {  
-  static getWeather(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-      .then(function(response) {
-        if (!response.ok) {
+  static async getWeather(city) {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
+      if (!response.ok) {
           throw Error (response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
-}
+}     
+      
